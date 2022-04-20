8fa85d40212f024f97a001155d105b27d6801301
mikoto <mikoto@ungine.cn>
2022-04-20 13:08:37 +0800

    (init)[[aerfaying-hook-backend]: init

---
+++ src/main/java/net/mikoto/aerfaying/hook/AerfayingHookApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.aerfaying.hook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerfayingHookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AerfayingHookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mikoto/aerfaying/hook/AerfayingHookApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.aerfaying.hook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerfayingHookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
